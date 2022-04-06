6441ca45378c27bbe575688da4028f17294e32cd
lucianovilela <lucianovilela@gmail.com>
2022-04-06 19:24:21 +0000

    versao inicial

---
+++ src/main/java/com/vilela/geraecpf/App.java
@@ -0,0 +1,13 @@
+package com.vilela.geraecpf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/vilela/geraecpf/AppTest.java
@@ -0,0 +1,20 @@
+package com.vilela.geraecpf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
