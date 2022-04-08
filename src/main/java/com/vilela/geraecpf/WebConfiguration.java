b90907733bee3e2204643478fde91afaf7b0f214
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2022-04-08 08:20:58 -0300

    nova versao

--- src/main/java/com/vilela/geraecpf/WebConfiguration.java
+++ src/main/java/com/vilela/geraecpf/WebConfiguration.java
@@ -1,12 +1,21 @@
 package com.vilela.geraecpf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-
+	   @Override
+	   public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	       registry.addResourceHandler("/static/**")
+	               .addResourceLocations("classpath:/static")
+	               .addResourceLocations("file:/static")
+	               .addResourceLocations("classpath:/resources/static")
+	               .addResourceLocations("file:/static");
+	       registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	   }
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/index.html");
